8852490cf7ac30a54a7fdb55c685332f6bbbbc9e
Mintesnot Shikur <75339983+Mntua@users.noreply.github.com>
2024-09-20 12:25:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function addTasks() {
     let nameInputElement = document.querySelector('.js-name');
     let dateInputElement = document.querySelector('.js-date');
 
-    if(nameInputElement.value && dateInputElement.value != '') {
+    if(nameInputElement.value != '' && dateInputElement.value != '') {
         taskName.push(nameInputElement.value);
         taskDate.push(dateInputElement.value);
         document.querySelector('h2').style.color = "brown";
+        document.querySelector('.counter').innerHTML = taskName.length;
     }else {
         alert("Please enter task name and date!")
     }
@@ -19,15 +20,20 @@ function addTasks() {
     console.log(taskName, taskDate);
 }
 function unfinishedTasks() {
-    if(taskName.length === 0 && taskDate.length === 0) {
+    if(taskName.length == 0 || taskDate.length == 0) {
+        document.querySelector('.counter').innerHTML = taskName.length;
         document.querySelector('.unfinishedTasksIndicator').innerHTML = 'There is no unfinished tasks!';
         document.querySelector('h2').style.color = "rgb(23, 42, 77)";
+        document.querySelector('.tasks').innerHTML = '';
+        console.log(taskName.length)
     }else {
+        document.querySelector('.counter').innerHTML = taskName.length;
         let final = '';
         for(let i=0; i<taskName.length; i++) {
             final = final + `<div><p>${taskName[i]}</p></div>` + `<div><p>${taskDate[i]}</p></div>` + `<button class="delete-btn" onclick="
                 taskName.splice(${i}, 1);
-                unfinishedTasks();">Delete</button>`;
+                unfinishedTasks();
+            ">Delete</button>`;
         }
         document.querySelector('.tasks').innerHTML = final;
     }
